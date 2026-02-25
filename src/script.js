6cb140a518625820b22e2582d3abc7f46a9c9fd8
ThalisonGTN <tha.teixeira10@gmail.com>
2026-02-24 22:14:01 -0300

    Aula 1 Aula de JavaScript

---
+++ src/script.js
@@ -0,0 +1,13 @@
+// Exemplos de variavel let
+console.log('Testando Javascript');
+
+let nome = 'Thalison'
+let nome2 = 'Guilherme'
+
+console.log(nome);
+console.log(nome2);
+
+// Exemplos de variavel const
+const times = ['São paulo','Corinthians','Santos']
+
+console.log(times)
