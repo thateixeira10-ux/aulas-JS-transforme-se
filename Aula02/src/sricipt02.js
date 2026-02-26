a2090686b4d53827ab0fbb0cb95e6442264e2b2a
ThalisonGTN <tha.teixeira10@gmail.com>
2026-02-25 22:17:37 -0300

    operadores

---
+++ Aula02/src/sricipt02.js
@@ -0,0 +1,23 @@
+// Exemplos de opeações aritiméticas
+let a = 5;
+let x = (100 + 200) * a;
+
+console.log(x);
+
+// Exemplo de adição
+let texto1 = "Somente a dor coletiva ";
+texto1 += 'gera a união';
+
+console.log(texto1)
+
+// Exemplo de incremento
+let w = 5;
+w++;
+let z = w
+console.log(z)
+
+// Exemplo de Decremento
+let w = 5;
+b--;
+let e = b
+console.log(c)
