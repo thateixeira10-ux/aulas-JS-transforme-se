eddc82a89d1f8409c48b321dd28c3a105b6b5b4e
ThalisonGTN <tha.teixeira10@gmail.com>
2026-03-02 22:14:06 -0300

    Aula 05 - laço de repetição

---
+++ Aula05/src/script03.js
@@ -0,0 +1,19 @@
+//Introdução a Laços de repetição
+// for (let valor = 0; valor < 10; valor ++)
+// console.log(valor);
+
+//Exemplo de White loop
+let acumuladora = 0;
+
+
+// while (acumuladora <= 10) {
+//     console.log ("O valor da acumuladora aogra é de: " + acumuladora);
+// }
+
+//Exemplo de Do While Loop
+let acumuladora2 = 0;
+
+do{
+    console.log("O valor da acumuladora agora é de:" + acumuladora2);
+     acumuladora2 = acumuladora2 + 1;
+} while (acumuladora2 <=10)
